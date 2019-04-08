4137d9297a75e58c5f46681574586559279a6bd9
Your Name <you@example.com>
2019-04-08 19:56:09 +0300

    finish

--- soj1-exemplu1/src/main/java/main/Main.java
+++ soj1-exemplu1/src/main/java/main/Main.java
@@ -1,4 +1,17 @@
 package main;
 
+import config.ProjectConfig;
+import objects.Cat;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
+    public static void main() {
+        try(AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(ProjectConfig.class)) {
+            Cat c = context.getBean(Cat.class);
+            System.out.println(c.name);
+        }
+
+
+    }
 }
