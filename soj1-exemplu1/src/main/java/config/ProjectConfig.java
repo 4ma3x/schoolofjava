4a98aa0e476b614c41d1ac8abbbb3c0ff8cf28d4
Your Name <you@example.com>
2019-04-08 19:59:03 +0300

    finish2

--- soj1-exemplu1/src/main/java/config/ProjectConfig.java
+++ soj1-exemplu1/src/main/java/config/ProjectConfig.java
@@ -3,10 +3,13 @@ package config;
 import objects.Cat;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class ProjectConfig {
     @Bean
+    //vo fi aceeasi referinta
+    //@Scope("prototype")
     public Cat cat() {
         Cat c = new Cat();
         c.name = "tom";
--- soj1-exemplu1/src/main/java/objects/Cat.java
+++ soj1-exemplu1/src/main/java/objects/Cat.java
@@ -1,5 +1,9 @@
 package objects;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+
 public class Cat {
     public String name;
 }
